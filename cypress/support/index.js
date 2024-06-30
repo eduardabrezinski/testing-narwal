939bc6d2e4488ff0ae85c085c52d88778a1a263f
Eduarda Tester <eduarda.tester@gmail.com>
2024-06-30 20:13:00 -0300

    commit

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -2,5 +2,6 @@ import 'faker';
 
 Cypress.on('uncaught:exception', (err, runnable) => {
   // Retorna false para prevenir que o Cypress falhe o teste em qualquer exceção não tratada
+  //teste
   return false
 })
